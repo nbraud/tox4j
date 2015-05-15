tox4j/deprecated-tox4j

im.tox.tox4j.Tox{Core,Av}Impl -> im.tox.tox4j.impl.Tox{Core,Av}Native

src/main/cpp/tox4j/Tox4j.h
@@ -43,7 +43,7 @@ tox4j_fatal (JNIEnv *env, char const *message)
 
 
 #define METHOD(TYPE, NAME, ...) \
-extern "C" JNIEXPORT TYPE JNICALL CAT(Java_im_tox_tox4j_, CAT(CLASS, CAT(Impl_, NAME))) \
+extern "C" JNIEXPORT TYPE JNICALL CAT(Java_im_tox_tox4j_impl_, CAT(CLASS, CAT(Impl_, NAME))) \
   (JNIEnv *env, jclass, __VA_ARGS__)
 
 #define TOX_METHOD(TYPE, NAME, ...) METHOD(TYPE, CAT(PREFIX, NAME), __VA_ARGS__)

nbraud/tox4j

src/main/cpp/tox4j/Tox4j.h
@@ -6,7 +6,7 @@
 
 
 #define METHOD(TYPE, NAME, ...) \
-extern "C" JNIEXPORT TYPE JNICALL PP_CAT(Java_im_tox_tox4j_, PP_CAT(CLASS, PP_CAT(Impl_, NAME))) \
+extern "C" JNIEXPORT TYPE JNICALL PP_CAT(Java_im_tox_tox4j_impl_, PP_CAT(CLASS, PP_CAT(Native_, NAME))) \
   (JNIEnv *env, jclass, __VA_ARGS__)
 
 #define TOX_METHOD(TYPE, NAME, ...) METHOD(TYPE, PP_CAT(PREFIX, NAME), __VA_ARGS__)