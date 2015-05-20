0eebd3ae9742f2384e8094d23a0585d6829a669f
Nicolas Braud-Santoni <nicolas.braud-santoni@iaik.tugraz.at>
2015-05-21 00:18:03 +0200

    Message: Fixup (make abstract)

--- src/main/java/im/tox/hlapi/message/Message.scala
+++ src/main/java/im/tox/hlapi/message/Message.scala
@@ -2,7 +2,7 @@ package im.tox.hlapi.message
 
 import im.tox.hlapi.core.User
 
-class Message {
-  val from: User = ???
-  val time: Nothing = ??? // Need to pick a datatype for time
+abstract class Message {
+  val from: User
+  val time: Nothing // Need to pick a datatype for time
 }
