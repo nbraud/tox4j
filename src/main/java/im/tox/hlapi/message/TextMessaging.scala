74e5cd0695d85abea30aefd2cb11bb764c38655c
Nicolas Braud-Santoni <nicolas.braud-santoni@iaik.tugraz.at>
2015-05-27 00:56:07 +0200

    TextMessaging: Trivial conversion to ToxModule

--- src/main/java/im/tox/hlapi/message/TextMessaging.scala
+++ src/main/java/im/tox/hlapi/message/TextMessaging.scala
@@ -2,7 +2,11 @@ package im.tox.hlapi.message
 
 import im.tox.hlapi.core._
 
-class TextMessaging {
+class TextMessaging extends ToxModule {
+  type State = Unit
+  def initial = ()
+  def name = "TextMessaging"
+
   def startConversation(user: User)(tox: ToxState): (ToxState, UserConversation) = {
     ???
   }
