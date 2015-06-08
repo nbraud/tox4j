35be66c4f356f75c547da40646e7bbe0b59b7a4d
Nicolas Braud-Santoni <nicolas+tox@braud-santoni.eu>
2015-06-08 22:00:13 +0200

    FileLikeBench: Range over file sizes

--- src/test/java/im/tox/hlapi/storage/bench/FileLikeBenchWip.scala
+++ src/test/java/im/tox/hlapi/storage/bench/FileLikeBenchWip.scala
@@ -3,29 +3,36 @@ package im.tox.hlapi.storage.bench
 import im.tox.tox4j.bench.{ Confidence, TimingReport }
 import org.scalameter.api._
 
-import java.io.RandomAccessFile
+import java.io.{ File, RandomAccessFile }
 import java.nio.channels.FileChannel.MapMode
 import java.nio.MappedByteBuffer
 import im.tox.hlapi.storage.{ MappedByteBuffer => Buffer }
 
 class FileLikeBench extends TimingReport {
-  protected override def confidence = Confidence.extreme
+  protected override def confidence = Confidence.high
 
-  val fileLike = for {
-    // That's dirty
-    unit <- Gen.unit("")
+  val fileLike = (for {
+    size <- Gen.range("size (Mb)")(1, 100, 1)
   } yield {
-    val file = new RandomAccessFile("/tmp/tox4j-FileLike-bench", "rw")
-    file.setLength(1024 * 1024)
-    val buffer = Buffer(file.getChannel().map(MapMode.READ_WRITE, 0, 1024 * 1024))
-    buffer
-  }
+    val file = new File(s"/tmp/tox4j-FileLike-$size")
+    file.deleteOnExit()
+    val file2 = new RandomAccessFile(file, "rw")
+    file2.setLength(size * 1024 * 1024)
+    Buffer(file2.getChannel().map(MapMode.READ_WRITE, 0, size * 1024 * 1024))
+  }).cached
 
   timing of "FileLike" in {
     measure method "get" in {
       using(fileLike) in {
         _.get(10)
       }
+
+      using(fileLike) in (x => {
+        for (i <- 0.toLong to x.size) {
+          x.get(i)
+        }
+      })
+
     }
 
     measure method "get-flush" in {
@@ -40,6 +47,12 @@ class FileLikeBench extends TimingReport {
       using(fileLike) in {
         _.set(10, 0x42: Byte)
       }
+
+      using(fileLike) in (x => {
+        for (i <- 0.toLong to x.size) {
+          x.set(i, i.toByte)
+        }
+      })
     }
 
     measure method "set-flush" in {
