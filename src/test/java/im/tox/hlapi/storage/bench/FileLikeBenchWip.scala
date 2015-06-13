4cfc5cd46bb7f363e09214d5c359633c5cbf86f3
Nicolas Braud-Santoni <nicolas+tox@braud-santoni.eu>
2015-06-13 15:20:18 +0200

    FileLikeBenchWip: update

--- src/test/java/im/tox/hlapi/storage/bench/FileLikeBenchWip.scala
+++ src/test/java/im/tox/hlapi/storage/bench/FileLikeBenchWip.scala
@@ -9,35 +9,42 @@ import java.nio.MappedByteBuffer
 import im.tox.hlapi.storage.{ MappedByteBuffer => Buffer }
 
 class FileLikeBench extends TimingReport {
-  protected override def confidence = Confidence.high
+  //  protected override def confidence = Confidence.high
 
   val fileLike = (for {
-    size <- Gen.range("size (MB)")(1, 25, 1)
+    size <- Gen.range("size (KB)")(100, 10500, 400)
   } yield {
     val file = new File(s"/tmp/tox4j-FileLike-$size")
     file.deleteOnExit()
     val file2 = new RandomAccessFile(file, "rw")
-    file2.setLength(size * 1024 * 1024)
-    Buffer(file2.getChannel().map(MapMode.READ_WRITE, 0, size * 1024 * 1024))
+    file2.setLength(size * 1024)
+    Buffer(file2.getChannel().map(MapMode.READ_WRITE, 0, size * 1024))
   }).cached
-
 
   timing of "FileLike" in {
 
     measure method "get" in {
       using(fileLike) in (x => {
-        for (i <- 0.toLong to x.size - 1) {
-          x.get(i)
+        val slice = x(0.toLong, x.size.toInt).orNull
+        for (i <- 0 to x.size.toInt - 1) {
+          slice.get(i)
         }
       })
     }
 
-
     measure method "set" in {
       using(fileLike) in (x => {
-        for (i <- 0.toLong to x.size - 1) {
-          x.set(i, i.toByte)
+        val slice = x(0.toLong, x.size.toInt).orNull
+        for (i <- 0 to x.size.toInt - 1) {
+          slice.set(i, i.toByte)
         }
+      })
+    }
+
+    measure method "iterate" in {
+      using(fileLike) in (x => {
+        val slice = x(0.toLong, x.size.toInt).orNull
+        slice.foreach(c => ())
       })
     }
 
